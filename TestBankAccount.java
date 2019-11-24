0d3c673b91c100023e6961a65faf937b3e288d00
veenatiwari2020 <58107836+veenatiwari2020@users.noreply.github.com>
2019-11-24 12:24:03 +0530

    Update TestBankAccount.java
    test add

--- TestBankAccount.java
+++ TestBankAccount.java
@@ -1,9 +1,15 @@
-package ca.uwo.csd.cs2212.USERNAME;
+package ca.uwo.csd.cs2212.veena_tiwari;
 
 import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestBankAccount {
+  @Test
+  public void testDebitWthSufficientFunds(){
+  BankAccount account =new BankAccount(10);
+    double amount =account.debit(5);
+    Assert.assertEquals(5.0,amount);
+  }
 
 }
